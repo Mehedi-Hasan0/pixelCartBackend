3f9abefd81a5f4912e1239d478595ca31e4d4cd3
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-02-05 10:53:06 +0600

    Gobal error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { UserRoutes } from './app/modules/user/user.route';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 
@@ -13,6 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use('/api/v1/users', UserRoutes);
+
+// global error handler
+app.use(globalErrorHandler);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is working for pixel perfect');
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,46 @@
+import { ErrorRequestHandler } from 'express';
+import { IGenericErrorMessage } from '../../types';
+import handleValidationError from '../../errors/handleValidationError';
+import config from '../../config';
+import ApiError from '../../errors/ApiError';
+
+const globalErrorHandler: ErrorRequestHandler = async (
+  error,
+  req,
+  res,
+  next,
+) => {
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorMessages: IGenericErrorMessage[] = [];
+
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorMessages = error?.message
+      ? [{ path: '', message: error?.message }]
+      : [];
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode;
+    message = error?.message;
+    errorMessages = error?.message
+      ? [{ path: '', message: error?.message }]
+      : [];
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? error?.stack : undefined,
+  });
+
+  next();
+};
+
+export default globalErrorHandler;
