e6a719ad29155a33b5bbce6dda882c050af5cfa5
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-04-25 19:20:25 +0600

    password hashing done

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { Schema, model } from 'mongoose';
 import { IUser, UserModel } from './user.interface';
+import bcrypt from 'bcrypt';
+import config from '../../../config';
 
 const userSchema = new Schema<IUser, UserModel>(
   {
@@ -42,5 +45,17 @@ const userSchema = new Schema<IUser, UserModel>(
     },
   },
 );
+
+// hashing password
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  next();
+});
 
 export const User = model<IUser, UserModel>('User', userSchema);
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,5 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.SALT_ROUNDS,
 };
